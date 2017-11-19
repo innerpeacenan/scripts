4d3a7e6cad70a2b9597c7a1330c699a36decf9ed
innerpeace <909312359@qq.com>
2017-11-19 11:42:58 +0800

    php error handler

--- php/http.php
+++ php/http.php
@@ -1,6 +1,31 @@
 <?php
-$html = file_get_contents('http://www.baidu.com/');
-print_r($http_response_header);
-$fp = fopen('http://www.baidu.com/',r);
-print_r(stream_get_meta_data($fp));
-?>
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+set_error_handler('exceptions_error_handler');
+
+function exceptions_error_handler($severity, $message, $filename, $lineno) {
+    if (error_reporting() == 0) {
+        return;
+    }
+    if (error_reporting() & $severity) {
+        throw new ErrorException($message, 0, $severity, $filename, $lineno);
+    }
+}
+
+
+try{
+    $html = file_get_contents('http://www.baidu.com/');
+    //print_r($http_response_header);
+    $fp = fopen('http://www.baidu.com/',r);
+    print_r(stream_get_meta_data($fp));
+}catch(Exception $e){
+    $stack = xdebug_get_function_stack();
+    var_export($stack);
+    echo PHP_EOL;exit();
+    echo $e->getMessage();
+    echo PHP_EOL. " at ";
+    echo $e->getFile() . ":" . $e->getLine();
+    echo PHP_EOL;
+    exit();
+}
--- php/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-$condition = new stdClass();
-$condition->name = 'thinkphp';
-$condition->status= 1;
-var_export($condition);
-?>
